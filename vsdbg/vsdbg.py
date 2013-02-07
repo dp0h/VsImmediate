c6eecda68d43ae9231523991625f9c39c0d4398e
dp0h <andrei.visnakovs@gmail.com>
2013-02-07 14:43:45 +0000

    Changed python script to make it reconnect every time.

--- vsdbg/vsdbg.py
+++ vsdbg/vsdbg.py
@@ -3,64 +3,58 @@
 import socket
 
 
-DEFAULT_HOST = "localhost"
-
 class ReqType:
     VALUE = 1
     TYPE = 2
     MEMBERS = 3
     ALL = 4
 
+BEL = '\x07'
 
-class VSDbgConnector(object):
-    def __init__(self, host, port):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.connect((host, port))
-
-    def __del__(self):
-        self._sock.close()
-
-    def call(self, type, expr):
-        self._sock.sendall('%d|%s' % (type, expr))
-        return self._sock.recv(4096)
+_port = 0
+_host = "localhost"
 
 
-_dbgConn = None
+def _dbg_call(tp, expr):
+    global _host, _port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((_host, _port))
+    s.sendall('%d%s%s' % (tp, BEL, expr))
+    res = s.recv(4096)
+    s.close()
+    res = res.split(BEL)
+    if len(res) < 2:
+        raise Exception('Invalid respose')
+    if int(res[0]) == 1:
+        raise Exception(res[1])
+    return res[1]
 
 
-def conn(port, host=DEFAULT_HOST):
-    ''' Connect to VS add-in '''
-    global _dbgConn
-    _dbgConn = VSDbgConnector(host, port)
+def port(p):
+    global _port
+    _port = p
+
+
+def host(h):
+    global _host
+    _host = h
 
 
 def p(expr):
     ''' retrieves expression '''
-    global _dbgConn
-    if _dbgConn is None:
-        return "Connection isn't initialized."
-    return _dbgConn.call(ReqType.ALL, expr)
+    return _dbg_call(ReqType.ALL, expr)
 
 
 def v(expr):
     ''' retrieves expression's value '''
-    global _dbgConn
-    if _dbgConn is None:
-        return "Connection isn't initialized."
-    return _dbgConn.call(ReqType.VALUE, expr)
+    return _dbg_call(ReqType.VALUE, expr)
 
 
 def t(expr):
     ''' retrieves expression's type '''
-    global _dbgConn
-    if _dbgConn is None:
-        return "Connection isn't initialized."
-    return _dbgConn.call(ReqType.TYPE, expr)
+    return _dbg_call(ReqType.TYPE, expr)
 
 
 def m(expr):
     ''' retrieves list of expression's members '''
-    global _dbgConn
-    if _dbgConn is None:
-        return "Connection isn't initialized."
-    return _dbgConn.call(ReqType.MEMBERS, expr)
+    return _dbg_call(ReqType.MEMBERS, expr)
